2c7c282c7c7c55af0ff161bb15fbb5c9a7355209
Anurag Upadhyay <anuragfx818@gmail.com>
2023-10-29 17:44:57 +0530

    changes done in the books sections of the page

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
-const downloadRef = document.getElementsByClassName('download-button')[0];
-// downloadRef = document.getElementsByClassName('download-button2')[0];
 
 
-function download(){
-    downloadRef.innerHTML = 'Downloading';
+// const downloadRef = document.getElementById('download-button');
 
-}
+// function download(){
+//     downloadRef.textContent = 'Downloading';
+// }
