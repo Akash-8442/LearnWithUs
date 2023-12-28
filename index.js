0fb0f666f2b1127366bb1b89ed40a356b79e2b58
Anurag Upadhyay <anuragfx818@gmail.com>
2023-12-28 14:51:34 +0530

    dark mode in homepage

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
+var html = document.getElementsByTagName("html")[0];
+var courseBorder = document.getElementsByClassName("new-message-forhtml");
+var headerH1 = document.getElementById("learn-with-us");
+var courseText = document.getElementById("courses");
+var navbar = document.getElementById("navbar");
+var navbarText = document.getElementsByClassName("navbarA");
 
 
 
-// const downloadRef = document.getElementById('download-button');
+var mode1 = document.getElementById("mode1");
+var mode2 = document.getElementById("mode2");
 
-// function download(){
-//     downloadRef.textContent = 'Downloading';
-// }
+mode1.style.display = "none"
+
+function toggleModeLight(){
+    // html.style.background = "blue"
+    html.style.background = "rgb(170, 231, 255)"
+    html.style.color = "black"
+    mode2.style.display = "block"
+    headerH1.style.background = "rgb(20, 97, 164)";
+    courseText.style.background = "rgb(91, 174, 245)";
+    navbar.style.background = "rgb(74, 182, 250)";
+}
+
+function toggleModeDark(){
+    html.style.background = "black";
+    html.style.color = "white";
+    
+    for (let index = 0; index < courseBorder.length; index++) {
+        const element = courseBorder[index];
+        element.style.border = "1px solid white";   
+    }
+    
+    for (let index = 0; index < navbarText.length; index++) {
+        const element = navbarText[index];
+        element.style.color = "white"
+        
+    }
+    
+    headerH1.style.background = "black";
+    navbar.style.background = "black";
+    navbar.style.color = "white";
+    courseText.style.background = "black";
+
+    mode2.style.display = "none";
+    mode1.style.display = "block";
+}
+
+
+mode1.addEventListener("click" , toggleModeLight)
+mode2.addEventListener("click" , toggleModeDark)
