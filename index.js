ed5978f6dc55c7b492a8fd93d37db63682d2011b
Anurag Upadhyay <anuragfx818@gmail.com>
2023-12-29 12:57:33 +0530

    Dark Mode Changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var navbar = document.getElementById("navbar");
 var navbarText = document.getElementsByClassName("navbarA");
 
 
-
 var mode1 = document.getElementById("mode1");
 var mode2 = document.getElementById("mode2");
 
@@ -20,32 +19,53 @@ function toggleModeLight(){
     headerH1.style.background = "rgb(20, 97, 164)";
     courseText.style.background = "rgb(91, 174, 245)";
     navbar.style.background = "rgb(74, 182, 250)";
+
+    for (let index = 0; index < navbarText.length; index++) {
+        const element = navbarText[index];
+        element.style.color = "black"  
+    }
+
+    for (let index = 0; index < courseBorder.length; index++) {
+        const element = courseBorder[index];
+        element.style.border = "1px solid black";   
+    }
+    courseText.style.color = "white";
+    headerH1.style.color = "white";
+    headerH1.style.borderLeft = "0.6rem solid white";
+    headerH1.style.borderRight = "0.6rem solid white";
+    headerH1.style.borderBottom = "0.6rem solid white";
+
 }
 
 function toggleModeDark(){
     html.style.background = "black";
-    html.style.color = "white";
+    html.style.color = "rgb(74, 182, 250)";
     
     for (let index = 0; index < courseBorder.length; index++) {
         const element = courseBorder[index];
-        element.style.border = "1px solid white";   
+        element.style.border = "1px solid rgb(74, 182, 250)";  
     }
     
     for (let index = 0; index < navbarText.length; index++) {
         const element = navbarText[index];
-        element.style.color = "white"
-        
+        element.style.color = "rgb(74, 182, 250)"  
     }
+    // You forgot to change it back when user return to light mode
+    // Do that and then only remove this comment !
     
     headerH1.style.background = "black";
+    headerH1.style.color = "rgb(74, 182, 250)";
+    headerH1.style.borderLeft = "0.6rem solid rgb(74, 182, 250)";
+    headerH1.style.borderRight = "0.6rem solid rgb(74, 182, 250)";
+    headerH1.style.borderBottom = "0.6rem solid rgb(74, 182, 250)";
     navbar.style.background = "black";
-    navbar.style.color = "white";
+    navbar.style.color = "rgb(74, 182, 250)";
     courseText.style.background = "black";
+    courseText.style.color = "rgb(74, 182, 250)";
 
     mode2.style.display = "none";
     mode1.style.display = "block";
 }
-
 
 mode1.addEventListener("click" , toggleModeLight)
 mode2.addEventListener("click" , toggleModeDark)
